937b609a2803cd11ef968ec553f8c97fb33ba470
JavierPuma-Cod <codigos.jp.2002@gmail.com>
2026-03-09 20:03:45 -0500

    project creation and first exercise

---
+++ MoureDev/src/main/java/org/example/E001_HelloWord.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class E001_HelloWord
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Word");
+        //Print your name
+        System.out.println("My name is Javier Puma");
+        //Print in 2 lines Hello Word
+        System.out.print("Hello\nWord\n");
+        //Print your age, favorite color and your city
+        System.out.println("My name is Javier\nMy favorite color is Green\nMy city is Arequipa");
+    }
+}
