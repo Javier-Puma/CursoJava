4bb50ff53356bca1402442facf7fb3c538fa55a8
JavierPuma-Cod <codigos.jp.2002@gmail.com>
2026-03-09 20:28:29 -0500

    second exercise -> varibles

---
+++ MoureDev/src/main/java/org/example/E002_Variables.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class E002_Variables
+{
+    static void main(String[] args){
+        //Variables types
+        String S="text container";
+        //numerical data
+        int I=12;
+        Double D=2.2;
+        //Booleans
+        Boolean B=true;
+        //VAR, variable without defined type
+        var V01="Hello Word";
+        var V02=25;
+        //Calls
+        System.out.println(S);
+        System.out.println(I);
+        System.out.println(D);
+        System.out.println(B);
+        System.out.println(V01);
+        System.out.println(V02);
+    }
+}
